f8e6e5f72aaba5cfe5162a181068f4015eb849eb
ishanbhise0406 <ishan.bhise.btech2024@sitpune.edu.in>
2026-03-25 09:26:09 +0530

    Update Vehicle.java

--- exp2/Vehicle.java
+++ exp2/Vehicle.java
@@ -1,65 +1,81 @@
-public class VectorClass {
-    double[] comps;
-
-    public VectorClass(double[] arr) throws InvalidVectorException {
-        if (arr == null || arr.length == 0)
-            throw new InvalidVectorException("Invalid Vector");
-
-        // FIXED condition
-        if (arr.length != 2 && arr.length != 3)
-            throw new InvalidVectorException("Only 2D or 3D supported");
-
-        comps = arr.clone();
+import java.time.Year;
+public class Vehicle {
+    // Public data members
+    public String brandName;
+    public String modelName;
+    public double price;
+    public String color;
+    public Year mfgYear;
+    public char fuelType; // P-Petrol, D-Diesel, E-Electric, H-Hybrid, C-CNG
+    // Private data members
+    private String mfgCode;
+    private String regNo;
+    // Getters and setters
+    public void setMfgCode(String mCode) {
+        mfgCode = mCode;
     }
-
-    public VectorClass add(VectorClass v) throws InvalidVectorException {
-        checkDimension(v);
-        double results[] = new double[comps.length];
-
-        for (int i = 0; i < comps.length; i++) // FIXED
-            results[i] = comps[i] + v.comps[i];
-
-        return new VectorClass(results);
+    public String getMfgCode() {
+        return mfgCode;
     }
-
-    public VectorClass substract(VectorClass v) throws InvalidVectorException {
-        checkDimension(v);
-        double results[] = new double[comps.length];
-
-        for (int i = 0; i < comps.length; i++) // FIXED
-            results[i] = comps[i] - v.comps[i];
-
-        return new VectorClass(results);
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
     }
-
-    public double dotProduct(VectorClass v) throws InvalidVectorException {
-        checkDimension(v);
-        double result = 0;
-
-        for (int i = 0; i < comps.length; i++) // FIXED
-            result += comps[i] * v.comps[i]; // FIXED
-
-        return result;
+    public String getRegNo() {
+        return regNo;
     }
-
-    void checkDimension(VectorClass v) throws InvalidVectorException {
-        // FIXED comparison
-        if (comps.length != v.comps.length)
-            throw new InvalidVectorException("Incompatible Vector Dimension");
+    // Default constructor
+    public Vehicle() {
+        brandName = "Hyundai";
+        modelName = "i10";
+        price = 500000;
+        color = "Silver";
+        mfgYear = Year.of(2020);
+        fuelType = 'P';
+        mfgCode = "HYN12345";
+        regNo = "MH12AA1111";
     }
-
-    public void print() {
-        if (comps.length == 2)
-            System.out.print("Vector(" + comps[0] + ", " + comps[1] + ")");
-        else
-            System.out.print("Vector(" + comps[0] + ", " + comps[1] + ", " + comps[2] + ")");
+    // Parameterized constructor
+    public Vehicle(String brandName, String modelName, double price, String mfgCode) {
+        this.brandName = brandName;
+        this.modelName = modelName;
+        this.price = price;
+        this.mfgCode = mfgCode;
     }
-
-    @Override
-    public String toString() {
-        if (comps.length == 2)
-            return "Vector(" + comps[0] + ", " + comps[1] + ")";
-        else
-            return "Vector(" + comps[0] + ", " + comps[1] + ", " + comps[2] + ")";
+    // Copy constructor
+    public Vehicle(Vehicle v) {
+        brandName = v.brandName;
+        modelName = v.modelName;
+        price = v.price;
+        color = v.color;
+        mfgYear = v.mfgYear;
+        fuelType = v.fuelType;
+        mfgCode = v.mfgCode;
+        regNo = v.regNo;
+    }
+    // Required methods
+    public void start() {
+        System.out.println("Vehicle started");
+    }
+    public void stop() {
+        System.out.println("Vehicle stopped");
+    }
+    public void drive() {
+        System.out.println("Vehicle is driving");
+    }
+    public void changeSpeed(int newSpeed) {
+        System.out.println("Speed changed to " + newSpeed);
+    }
+    // Mileage depends on fuel type and distance
+    public double calcMileage(double distance) {
+        double efficiency;
+        switch (fuelType) {
+            case 'P': efficiency = 15; break;   // Petrol
+            case 'D': efficiency = 20; break;   // Diesel
+            case 'E': efficiency = 120; break;  // Electric
+            case 'H': efficiency = 25; break;   // Hybrid
+            case 'C': efficiency = 22; break;   // CNG
+            default: efficiency = 0;
+        }
+        return distance / efficiency;
     }
 }
