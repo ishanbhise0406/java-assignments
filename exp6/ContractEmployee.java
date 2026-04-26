c6fcf6b444afead7c19b36b6af82e0c8f3039e20
ishanbhise0406 <ishan.bhise.btech2024@sitpune.edu.in>
2026-04-26 14:57:22 +0530

    Update ContractEmployee.java

--- exp6/ContractEmployee.java
+++ exp6/ContractEmployee.java
@@ -1,1 +1,17 @@
+class ContractEmployee extends Employee {
+    int noOfHours;
+    double hourlyRate;
 
+    ContractEmployee(int employeeId, String name, String PAN, String joiningDate,
+                     String designation, int deptId, String role,
+                     int noOfHours, double hourlyRate) {
+        super(employeeId, name, PAN, joiningDate, designation, deptId, role);
+        this.noOfHours = noOfHours;
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    double calcCTC(Payroll p) {
+        return noOfHours * hourlyRate;
+    }
+}
